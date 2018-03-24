e5bf15dc06ca90117c679736043ddc4b567a51e1
Naruu <esara2021@gmail.com>
2018-03-24 10:56:49 +0900

    Error : Reverse for 'post_detail' with keyword arguments '{'pk': 1}' not found. 1 pattern(s) tried: ['$post\\/(?P<pk>[^/]+)\\/$']

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    re_path(r'', views.post_list, name = 'post_list'),
+    path('', views.post_list, name = 'post_list'),
+    path('post/<pk>/', views.post_detail, name = 'post_detail'),
 ]
